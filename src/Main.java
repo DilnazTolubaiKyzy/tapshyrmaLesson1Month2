16fd69a610cb5ea24692cbfea9b69a4a956967db
леново <tolubaikyzydilnaz@gmail.com>
2022-10-04 12:56:31 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.awt.geom.Area;
+
+public class Main {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle();
+        triangle.a = 3;
+        triangle.b = 4;
+        triangle.c = 5;
+
+        triangle.area();
+    }
+}
---
+++ src/Triangle.java
@@ -0,0 +1,13 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Triangle {
+
+    int a;
+    int b;
+    int c;
+
+    public void area() {
+        System.out.println("S = "+a*b*c );
+    }
+
+}
